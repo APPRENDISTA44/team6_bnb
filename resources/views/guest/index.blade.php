78bcff616f59ec31b4309527a40d350966044edf
Luca Isidoro <apprendista44@gmail.com>
2020-09-28 11:34:02 +0200

    correzione index.blade.php

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -3,13 +3,9 @@
   <div class="container" id="ms_homepage">
     <div class="row">
       <div class="col">
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success my-2 my-sm-0" id="ms_search_button" type="submit">Search</button>
-            </form>
-          </nav>
-        </div>
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" id="ms_search_button" type="submit">Search</button>
+      </div>
     </div>
   </div>
 
