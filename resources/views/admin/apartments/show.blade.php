8610697159ee910a6b85e848469296b3a4f75be0
Luca Isidoro <apprendista44@gmail.com>
2020-10-01 10:00:45 +0200

    inserita mappa

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -39,6 +39,36 @@
       </div>
     </div>
 
+    {{-- mappa --}}
+    <div class="col">
+
+      <h3>My Google Maps Demo</h3>
+    <!--The div element for the map -->
+    <div id="map"></div>
+    <script>
+      // Initialize and add the map
+      function initMap() {
+        // The location of Uluru
+        var uluru = {lat: {{$apartment->latitude}}, lng: {{$apartment->longitude}}};
+        // The map, centered at Uluru
+        var map = new google.maps.Map(
+          document.getElementById('map'), {zoom: 9, center: uluru});
+          // The marker, positioned at Uluru
+          var marker = new google.maps.Marker({position: uluru, map: map});
+        }
+    </script>
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+    <script defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCamGPsnxU7BzZrAKAsINUCAwh1ll8NFFc&callback=initMap">
+    </script>
+
+
+    </div>
+
   </div>
 
 </div>
